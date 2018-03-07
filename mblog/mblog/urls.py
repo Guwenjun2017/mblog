7cd981d405439e16c0b5bacf05ed4b7034f21d5a
guwenjun <guwenjun2017@gmail.com>
2018-03-07 22:54:29 +0800

    0307

--- mblog/mblog/urls.py
+++ mblog/mblog/urls.py
@@ -1,4 +1,5 @@
 """mblog URL Configuration
+   mblog 的网址设置
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
@@ -13,14 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 #from django.urls import path
 from django.conf.urls import include,url
-from mainsite.views import homepage
+from django.contrib import admin
+from mainsite.views import homepage, showpost
 
 urlpatterns = [
         url(r'^admin/', admin.site.urls),
         url(r'^$', homepage),
+        url(r'^post/(\w+)$', showpost),
 ] 
 #urlpatterns = [
 #    path('admin/', admin.site.urls),
