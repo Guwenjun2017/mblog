09cf9e4e62530dce86904bd6a7cb47c585f39586
guwenjun <guwenjun2017@gmail.com>
2018-03-08 20:53:15 +0800

    first finish

--- mblog/mainsite/views.py
+++ mblog/mainsite/views.py
@@ -4,23 +4,6 @@ from django.shortcuts import render, redirect
 from datetime import datetime
 from .models import Post
 
-#def homepage(request):
-#    posts = Post.objects.all()
-#    post_lists = list()
-#    for count, post in enumerate(posts):
-#        post_lists.append("No.{}:".format(str(count)) + str(post) + "<br>")
-#    return HttpResponse(post_lists)
-
-#def homepage(request):
-#    posts = Post.objects.all()
-#    post_lists = list()
-#    for count, post in enumerate(posts):
-#        post_lists.append("No.{}:".format(str(count)) + str(post) + "<hr>")
-#        post_lists.append("<small>" + str(post.body.encode('utf-8'))\
-#                +"<small><br><br>")
-#    
-#    return HttpResponse(post_lists)
-
 def homepage(request):
     template = get_template('index.html')
     posts = Post.objects.all()
